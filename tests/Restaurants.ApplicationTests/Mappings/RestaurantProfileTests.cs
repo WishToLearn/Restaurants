aa3697402c6dbfe42e13e47309446dfbda163d66
Rahul.S <Rahul.S@NEUDESICBN5HT44>
2025-04-08 15:38:15 +0530

    folder struture & unit tests

---
+++ tests/Restaurants.ApplicationTests/Mappings/RestaurantProfileTests.cs
@@ -0,0 +1,118 @@
+﻿using Xunit;
+using AutoMapper;
+using Restaurants.Application.Commands.Resraurant;
+using Restaurants.Domain.Entities;
+using Restaurants.Application.DTOs;
+using FluentAssertions;
+
+namespace Restaurants.Application.Mappings.Tests
+{
+    public class RestaurantProfileTests
+    {
+        private IMapper _mapper;
+
+        public RestaurantProfileTests()
+        {
+            var configuration = new MapperConfiguration(config =>
+            {
+                config.AddProfile<RestaurantProfile>();
+            });
+
+            _mapper = configuration.CreateMapper();
+        }
+
+        [Fact()]
+        public void CreateMap_ForCreateRestaurantCommandToRestaurant_MapsCorrectly()
+        {
+            // arrange
+            var command = new CreateRestaurantCommand
+            {
+                Name = "Test Restaurant",
+                Description = "Test Description",
+                Category = "Test Category",
+                HasDelivery = true,
+                ContactEmail = "test@example.com",
+                ContactNumber = "0123456789",
+                City = "Test City",
+                Street = "Test Street",
+                PostalCode = "12-345"
+            };
+
+            // act
+            var restaurant = _mapper.Map<Restaurant>(command);
+
+            // assert 
+
+            restaurant.Should().NotBeNull();
+            restaurant.Name.Should().Be(command.Name);
+            restaurant.Description.Should().Be(command.Description);
+            restaurant.Category.Should().Be(command.Category);
+            restaurant.HasDelivery.Should().Be(command.HasDelivery);
+            restaurant.ContactEmail.Should().Be(command.ContactEmail);
+            restaurant.ContactNumber.Should().Be(command.ContactNumber);
+            restaurant.Address.Should().NotBeNull();
+            restaurant.Address.City.Should().Be(command.City);
+            restaurant.Address.Street.Should().Be(command.Street);
+            restaurant.Address.PostalCode.Should().Be(command.PostalCode);
+        }
+
+        [Fact()]
+        public void CreateMap_ForRestaurantToRestaurantDto_MapsCorrectly()
+        {
+            // arrange
+            var restaurant = new Restaurant()
+            {
+                Id = 12345,
+                Name = "Test Restaurant",
+                Description = "Test Description",
+                Category = "Test Category",
+                HasDelivery = true,
+                ContactEmail = "test@example.com",
+                ContactNumber = "0123456789",
+                Address = new Address
+                {
+                    City = "Test City",
+                    Street = "Test Street",
+                    PostalCode = "12-345"
+                }
+            };
+
+            // act
+            var restaurantDTO = _mapper.Map<RestaurantDTO>(restaurant);
+
+            // assert 
+            restaurantDTO.Should().NotBeNull();
+            restaurantDTO.Id.Should().Be(restaurant.Id);
+            restaurantDTO.Name.Should().Be(restaurant.Name);
+            restaurantDTO.Description.Should().Be(restaurant.Description);
+            restaurantDTO.Category.Should().Be(restaurant.Category);
+            restaurantDTO.HasDelivery.Should().Be(restaurant.HasDelivery);
+            restaurantDTO.City.Should().Be(restaurant.Address.City);
+            restaurantDTO.Street.Should().Be(restaurant.Address.Street);
+            restaurantDTO.PostalCode.Should().Be(restaurant.Address.PostalCode);
+        }
+
+        [Fact()]
+        public void CreateMap_ForUpdateRestaurantCommandToRestaurant_MapsCorrectly()
+        {
+            // arrange
+            var command = new UpdateRestaurantCommand
+            {
+                Id = 12345,
+                Name = "Updated Restaurant",
+                Description = "Updated Description",
+                HasDelivery = false
+            };
+
+            // act
+            var restaurant = _mapper.Map<Restaurant>(command);
+
+            // assert 
+            restaurant.Should().NotBeNull();
+            restaurant.Id.Should().Be(command.Id);
+            restaurant.Name.Should().Be(command.Name);
+            restaurant.Description.Should().Be(command.Description);
+            restaurant.HasDelivery.Should().Be(command.HasDelivery);
+        }
+    }
+}
---
+++ tests/Restaurants.ApplicationTests/Validators/Restaurant/CreateRestaurantCommandValidatorTests.cs
@@ -0,0 +1,93 @@
+﻿using FluentValidation.TestHelper;
+using Restaurants.Application.Commands.Resraurant;
+using Xunit;
+
+namespace Restaurants.Application.Validators.Restaurant.Tests
+{
+    public class CreateRestaurantCommandValidatorTests
+    {
+        [Fact()]
+        public void Validator_ForValidCommand_ShouldNotHaveValidationErrors()
+        {
+            // arrange
+            var command = new CreateRestaurantCommand()
+            {
+                Name = "Test Name",
+                Description = "Test Description",
+                Category = "Indian",
+                ContactEmail = "test@test.com",
+                PostalCode = "12-345",
+            };
+
+            var validator = new CreateRestaurantCommandValidator();
+
+            // act
+            var result = validator.TestValidate(command);
+
+            // assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact()]
+        public void Validator_ForInvalidCommand_ShouldHaveValidationErrors()
+        {
+            // arrange
+            var command = new CreateRestaurantCommand()
+            {
+                Name = "TR",
+                Category = "Italy",
+                ContactEmail = "@test.com",
+                PostalCode = "12345",
+            };
+
+            var validator = new CreateRestaurantCommandValidator();
+
+            // act
+            var result = validator.TestValidate(command);
+
+            // assert
+            result.ShouldHaveValidationErrorFor(c => c.Name);
+            result.ShouldHaveValidationErrorFor(c => c.Category);
+            result.ShouldHaveValidationErrorFor(c => c.ContactEmail);
+            result.ShouldHaveValidationErrorFor(c => c.PostalCode);
+        }
+        
+        [Theory()]
+        [InlineData("Indian")]
+        [InlineData("Mexican")]
+        [InlineData("American")]
+        [InlineData("Italian")]
+        [InlineData("Afghan")]
+        [InlineData("Chinese")]
+        public void Validator_ForValidCategory_ShouldNotHaveValidationErrorsForCategoryProperty(string category)
+        {
+            // arrange
+            var validator = new CreateRestaurantCommandValidator();
+            var command = new CreateRestaurantCommand { Category = category };
+
+            // act
+            var result = validator.TestValidate(command);
+
+            // assert
+            result.ShouldNotHaveValidationErrorFor(c => c.Category);
+        }
+
+        [Theory()]
+        [InlineData("12345")]
+        [InlineData("123-45")]
+        [InlineData("12 345")]
+        [InlineData("12-3 45")]
+        public void Validator_ForInvalidPostalCode_ShouldHaveValidationErrorsForPostalCodeProperty(string postalCode)
+        {
+            // arrange
+            var validator = new CreateRestaurantCommandValidator();
+            var command = new CreateRestaurantCommand { PostalCode = postalCode };
+
+            // act
+            var result = validator.TestValidate(command);
+
+            // assert
+            result.ShouldHaveValidationErrorFor(c => c.PostalCode);
+        }
+    }
+}
